f8f71d942a93ba6a2b58be0fe58e0db9cc0646c5
root <Olatunde Pedro>
2023-01-12 13:18:35 -0800

    1

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,8 +1,3 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
-search_replace = __import__('1-search_replace').search_replace
-my_list = [1, 2, 3, 4, 5, 4, 2, 1, 1, 4, 89]
-new_list = search_replace(my_list, 2, 89)
-
-print(new_list)
-print(my_lisit)
+    return (list(map(lambda x: replace if x is search else x, my_list)))
