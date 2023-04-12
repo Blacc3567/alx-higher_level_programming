77b60b7287f9641e47cc272e861eb1691b44a87a
Olatunde Pedro <blackkid306@gmail.com>
2023-04-12 17:15:15 +0000

    09

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,9 +1,23 @@
 #!/usr/bin/node
-if (process.argv.length <= 3) {
+if (process.argv.length < 4) {
   console.log(0);
 } else {
-  const args = process.argv.map(Number)
-    .slice(2, process.argv.length)
-    .sort((a, b) => a - b);
-  console.log(args[args.length - 2]);
+  let fbig, sbig;
+  if (parseInt(process.argv[2]) >= parseInt(process.argv[3])) {
+    fbig = parseInt(process.argv[2]);
+    sbig = parseInt(process.argv[3]);
+  } else {
+    fbig = parseInt(process.argv[3]);
+    sbig = parseInt(process.argv[2]);
+  }
+
+  for (let i = 4; i < process.argv.length; i++) {
+    if (parseInt(process.argv[i]) > fbig) {
+      sbig = fbig;
+      fbig = parseInt(process.argv[i]);
+    } else if (parseInt(process.argv[i]) > sbig) {
+      sbig = parseInt(process.argv[i]);
+    }
+  }
+  console.log(sbig);
 }
