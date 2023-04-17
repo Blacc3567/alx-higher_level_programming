07e96e70a2bf7ab2de92bf73b44aa083dd1c70df
root <Olatunde Pedro>
2023-04-17 04:06:53 -0700

    -

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -3,9 +3,6 @@
 import MySQLdb
 import sys
 
-CREATE DATABASE IF NOT EXISTS hbtn_0e_0_usa;
-USE hbtn_0e_0_usa;
-
 if __name__ == "__main__":
     db = MySQLdb.connect(host="localhost", user=sys.argv[1],
                          passwd=sys.argv[2], db=sys.argv[3], port=3306)
